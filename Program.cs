50b5ebce211851440aa057762d43dff516c48be9
Matheus <matheus.viee.47@gmail.com>
2025-04-04 12:52:36 -0300

    Aprendendo Listas e Data Structure

--- Program.cs
+++ Program.cs
@@ -6,45 +6,51 @@ using System.Threading.Tasks;
 
 namespace ConsoleProject
 {
-    class Program
+    internal class Program
     {
+        struct DadosCadastrais_Struct
+        {
+            public string Nome;
+            public string NomeDaRua;
+            public UInt32 NumeroDaCasa;
+            public string NumeroDoDocumento;
+        }
         static void Main(string[] args)
         {
+            List<DadosCadastrais_Struct> ListaDeCadastros = new List<DadosCadastrais_Struct>();
             string opcao;
             do
             {
-                Console.WriteLine("Pressionce C para cadastrar um usuário ou S para sair:");
+                Console.WriteLine("Digite C para cadastrar um novo usuário ou S para sair:");
                 opcao = Console.ReadKey(true).KeyChar.ToString().ToLower();
-                if(opcao == "c")
+                if (opcao == "c")
                 {
-                    Console.WriteLine("Digite o seu nome completo");
-                    string nome = Console.ReadLine();
-                    Console.WriteLine("Pressione M para masculino e F para Feminino");
-                    char genero = Console.ReadKey(true).KeyChar;
-                    Console.WriteLine("Digite a data de nascimento no formato dd/mm/aaaa");
-                    DateTime DataDeNascimento = Convert.ToDateTime(Console.ReadLine());
-                    Console.WriteLine("Digite o nome da sua rua");
-                    string nomeDaRua = Console.ReadLine();
-                    Console.WriteLine("Digite o número da casa");
-                    UInt32 numeroDaCasa = Convert.ToUInt32(Console.ReadLine());
+                    //Cadastrar novo usuário
+                    DadosCadastrais_Struct dadosCadastrais;
+                    Console.WriteLine("Digite o nome completo:");
+                    dadosCadastrais.Nome = Console.ReadLine();
+                    Console.WriteLine("Digite o nome da rua:");
+                    dadosCadastrais.NomeDaRua = Console.ReadLine();
+                    Console.WriteLine("Digite o número da casa:");
+                    dadosCadastrais.NumeroDaCasa = Convert.ToUInt32(Console.ReadLine());
+                    Console.WriteLine("Digite o número do documento");
+                    dadosCadastrais.NumeroDoDocumento = Console.ReadLine();
+                    ListaDeCadastros.Add(dadosCadastrais);
                     Console.Clear();
-
-
                 }
-                else if(opcao == "s")
+                else if (opcao == "s")
                 {
-                    Console.WriteLine("Encerrando o programa.");
+                    //encerrar
+                    Console.WriteLine("Encerrando a aplicação.");
                 }
                 else
                 {
+                    //opcao desconhecida
                     Console.WriteLine("Opção desconhecida.");
                 }
             } while (opcao != "s");
             Console.WriteLine("Pressione qualquer tecla para sair.");
             Console.ReadKey();
-
-
-        } 
+        }
     }
-
 }
